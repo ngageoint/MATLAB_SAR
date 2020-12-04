845bf7eea4d20e09ead6a6e3de215f427d2d7c34
Wade Schwartzkopf <Wade.C.Schwartzkopf.ctr@nga.mil>
2020-12-03 21:16:36 -0500

    logremap now self-contained

--- Visualization/remap/remap_funcs/logremap.m
+++ Visualization/remap/remap_funcs/logremap.m
@@ -5,10 +5,21 @@ function out = logremap(x)
 % /// CLASSIFICATION: UNCLASSIFIED       ///
 % //////////////////////////////////////////
 
-[out, disp_min, disp_max] = logremap_float(x);
-out = uint8(255*(out-disp_min)/(disp_max-disp_min));
+x = abs(single(x));
+x = 10*x./mean(x(:)); % Fixes very small data (<< 1)
+rcent  = 10*log10(sum(x(:).^2)/numel(x));
+% Make minimum at least one
+x = x + max(1 - min(x(:)),0);
+x = 20*log10(x);
+
+span_db = 50;
+disp_min = max(min(x(:)),rcent - span_db/2);
+disp_max = min(max(x(:)),rcent + span_db/2);
+
+out = uint8(255*(x-disp_min)/(disp_max-disp_min));
 
 end
+
 % //////////////////////////////////////////
 % /// CLASSIFICATION: UNCLASSIFIED       ///
 % //////////////////////////////////////////
